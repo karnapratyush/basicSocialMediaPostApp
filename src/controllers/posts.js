cca3c7de3a22f13ac86f3bd0dfe30c9048472c2f
karnapratyush <pratyushkumarkarna@gmail.com>
2020-11-23 23:30:25 +0530

    fea: added functionality of creating and showing all posts

---
+++ src/controllers/posts.js
@@ -0,0 +1,43 @@
+const { Users, Posts}=require('../db/models');
+
+
+async function createNewPost(userId,title,body){
+    const post=await Posts.create({
+        title,
+        body,
+        userId
+    })
+
+    return post;
+}
+
+
+async function showAllPosts(query)
+{
+    // todo  handle query params
+    const posts=await Posts.findAll({
+        include:[Users]
+    });
+    return posts;
+}
+
+
+// async function task(){
+//     // console.log(await createNewPost(1,'this is title','this is body of post'))
+
+
+// const posts=await showAllPosts()
+// for (let p of posts)
+// {
+//     console.log(`${p.title}\n${p.body}\n${p.user.username}\n======\n`)
+// }
+
+// }
+
+// task();
+
+
+module.exports={
+    createNewPost,
+    showAllPosts
+}
